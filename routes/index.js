4ab403a80429fcfb38c5dac0476c8ad781098295
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-01-01 21:09:41 -0800

    create basic pug file for testing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,29 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const peep = { name: "YO", age: 200, cool: true };
+  // res.send('Hey! It works!');
+  // res.json(peep);
+
+  //sends back 'name=' data entered into url query
+  // res.send(req.query.name);
+
+  //sends back url query input as a json string
+  //res.send(req.query) actually sends back the same thing...
+  // res.json(req.query);
+
+  res.render('hello', {
+    name: req.query.name,
+    dog: req.query.dog
+  });
+});
+
+// router example of handling url inputs
+// reverses name entered at '/reverse/_____'
+router.get('/reverse/:name', (req, res) => {
+  let name = [...req.params.name];
+  res.send(name.reverse().join("")) 
+  
 });
 
 module.exports = router;
