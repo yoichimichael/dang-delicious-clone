e1501371d53431d721da0cd3fbc4eefa1850d6e9
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-01-02 14:45:41 -0800

    create index.pug template and add res.render() function too storeController

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController')
 
 // Do work here
-router.get('/', (req, res) => {
-  const peep = { name: "YO", age: 200, cool: true };
-  // res.send('Hey! It works!');
-  // res.json(peep);
+router.get('/', storeController.homePage)
 
-  //sends back 'name=' data entered into url query
-  // res.send(req.query.name);
 
-  //sends back url query input as a json string
-  //res.send(req.query) actually sends back the same thing...
-  // res.json(req.query);
+// router.get('/', (req, res) => {
+   
+//   const peep = { name: "YO", age: 200, cool: true };
+//   res.send('Hey! It works!');
+//   res.json(peep);
 
-  res.render('hello', {
-    name: req.query.name,
-    dog: req.query.dog,
-    title: "I love ramen"
-  });
-});
+//   sends back 'name=' data entered into url query
+//   res.send(req.query.name);
+
+//   sends back url query input as a json string
+//   res.send(req.query) actually sends back the same thing...
+//   res.json(req.query);
+
+//   res.render('hello', {
+//     name: req.query.name,
+//     dog: req.query.dog,
+//     title: "I love ramen"
+//   });
+// });
 
 // router example of handling url inputs
 // reverses name entered at '/reverse/_____'
-router.get('/reverse/:name', (req, res) => {
-  let name = [...req.params.name];
-  res.send(name.reverse().join("")) 
+// router.get('/reverse/:name', (req, res) => {
+//   let name = [...req.params.name];
+//   res.send(name.reverse().join("")) 
   
-});
+// });
 
 module.exports = router;
