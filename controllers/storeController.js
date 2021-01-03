41be2015f418b8788579aea891528b2421a5889c
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-01-02 17:04:31 -0800

    test middleware and error handlers

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,17 @@
+exports.myMiddleware = (req, res, next) => {
+  req.name = "Yoichi";
+  if(req.name === 'Yoichi'){
+    throw Error('That is a stupid name');
+  }
+
+  // sets a cookie
+  // res.cookie('name', 'Yo is cool', { maxAge: 9000000000 })
+  next();
+}
+
 exports.homePage = (req, res) => {
+  console.log(req.name);
   res.render('index');
 }
+
+
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const storeController = require('../controllers/storeController')
 
 // Do work here
-router.get('/', storeController.homePage)
+
+router.get('/', storeController.myMiddleware, storeController.homePage)
 
 
 // router.get('/', (req, res) => {
