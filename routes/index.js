9a9033ccd00409335ce8032c7209451ccd6ebb40
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-01-12 23:34:25 -0500

    add addStore route to routes in index.js

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const storeController = require('../controllers/storeController')
 // Do work here
 
 router.get('/', storeController.homePage)
+router.get('/add', storeController.addStore)
 
 
 // router.get('/', (req, res) => {
