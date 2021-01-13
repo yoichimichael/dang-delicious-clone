72655ffeced5b8205454252679127306c462af5c
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-01-13 00:44:26 -0500

    add form to _storeForm mixin and add POST route to index.js

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -4,5 +4,6 @@ exports.homePage = (req, res) => {
 }
 
 exports.addStore = (req, res) => {
-  res.send("It works");
+  // used for both 'adding' and 'editing' a store to keep number of templates low and code DRY
+  res.render('editStore', { title: 'Add Store' });
 }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const storeController = require('../controllers/storeController')
+const storeController = require('../controllers/storeController');
 
 // Do work here
 
-router.get('/', storeController.homePage)
-router.get('/add', storeController.addStore)
+router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 
 // router.get('/', (req, res) => {
