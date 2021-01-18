26585db765d67916d817837eb0397e87d5b207e1
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-01-17 23:59:21 -0500

    add flash messaging for adding stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -4,6 +4,10 @@ const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
   console.log(req.name);
+  req.flash('error', 'Something Happened');
+  req.flash('info', 'Something Happened');
+  req.flash('warning', 'Something Happened');
+  req.flash('success', 'Something Happened');
   res.render('index');
 }
 
@@ -18,5 +22,7 @@ exports.createStore = async (req, res) => {
   // ... which we 'await'
   // code will stop until save has returned data or an error
   await store.save()
+
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
   res.redirect('/')
 } 
