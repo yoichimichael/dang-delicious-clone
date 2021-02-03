f328c0817b54b4f39af9c07850e9b44a94803569
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-02-02 23:59:00 -0500

    create userController file, first route and create login.pug file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 
 // object destructuring
 // only importing the catchErrors function from errorHandlers.js
@@ -32,6 +33,7 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
+router.get('/login', userController.loginForm);
 
 // router.get('/', (req, res) => {
    
