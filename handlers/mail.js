c8302f6e3dd093f0cf01a4ec517d698e9570cc73
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-02-07 20:20:31 -0500

    add mailtrap login data, create mail.js, import packages

---
+++ handlers/mail.js
@@ -0,0 +1,6 @@
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+const juice = require('juice');
+const htmlToText = require('html-to-text');
+const promisify = require('es6-promisify');
+
