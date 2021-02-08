834cc83ed2bcc02f4126770000f59e364067810c
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-02-07 20:24:30 -0500

    create transport object

--- handlers/mail.js
+++ handlers/mail.js
@@ -4,3 +4,11 @@ const juice = require('juice');
 const htmlToText = require('html-to-text');
 const promisify = require('es6-promisify');
 
+const transport = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
+  }
+})
