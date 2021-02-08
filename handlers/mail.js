97d2c6a7b0973ae62118cdccc6c55160df306820
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-02-07 20:35:43 -0500

    create send function in mail.js

--- handlers/mail.js
+++ handlers/mail.js
@@ -4,6 +4,7 @@ const juice = require('juice');
 const htmlToText = require('html-to-text');
 const promisify = require('es6-promisify');
 
+// setup to work with Mailtrap.io
 const transport = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
@@ -11,4 +12,24 @@ const transport = nodemailer.createTransport({
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
   }
-})
+});
+
+exports.send = async (options) => {
+  const mailOptions = {
+    from: 'Yoichi Nagano <ymnagano@protonmail.com',
+    to: options.user.email,
+    subject: options.subject, 
+    html: 'This will be filled in later',
+    text: 'This will also be filled in later'
+  };
+  const sendMail = promisify(transport.sendMail, transport);  
+  return sendMail(mailOptions);
+}
+
+// transport.sendMail({
+//   from: 'Yoichi Nagano <ymnagano@protonmail.com>',
+//   to: 'danielle.keita.taguchi@gmail.com',
+//   subject: 'Just trying things out!',
+//   html: 'Hey I <strong>love</strong> you',
+//   text: 'hey I **love** you'
+// });
