81e46673110fab679e598c79b4704cbb3cb7ab9b
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-02-10 23:07:34 -0500

    create api route for store mapping search

--- routes/index.js
+++ routes/index.js
@@ -68,6 +68,7 @@ router.post(
 */
 
 router.get('/api/search', catchErrors(storeController.searchStores));
+router.get('/api/stores/near', catchErrors(storeController.mapStores));
 
 
 // router.get('/', (req, res) => {
