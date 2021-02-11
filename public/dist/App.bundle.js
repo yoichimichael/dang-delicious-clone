2d880bfc349eb533469910dc101797554bcb4167
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-02-11 00:14:01 -0500

    create loadPlaces helper func to help populate stores on map

--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -2722,10 +2722,68 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 /***/ }),
 /* 32 */,
 /* 33 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-throw new Error("Module build failed: SyntaxError: Unexpected token (19:0)\n\n\u001b[0m \u001b[90m 17 | \u001b[39m  \u001b[36mconst\u001b[39m map \u001b[33m=\u001b[39m \u001b[36mnew\u001b[39m google\u001b[33m.\u001b[39mmaps\u001b[33m.\u001b[39m\u001b[33mMap\u001b[39m(mapDiv\u001b[33m,\u001b[39m mapOptions)\u001b[33m;\u001b[39m\n \u001b[90m 18 | \u001b[39m  \u001b[36mconst\u001b[39m inptut\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 19 | \u001b[39m}\n \u001b[90m    | \u001b[39m\u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 20 | \u001b[39m\n \u001b[90m 21 | \u001b[39m\n \u001b[90m 22 | \u001b[39m\u001b[36mexport\u001b[39m \u001b[36mdefault\u001b[39m makeMap\u001b[33m;\u001b[39m\u001b[0m\n");
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+var _axios = __webpack_require__(12);
+
+var _axios2 = _interopRequireDefault(_axios);
+
+var _bling = __webpack_require__(9);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var mapOptions = {
+  center: { lat: 43.2, lng: -79.8 },
+  zoom: 10
+};
+
+function loadPlaces(map) {
+  var lat = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 43.2;
+  var lng = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : -79.8;
+
+  _axios2.default.get('/api/stores/near?lat=' + lat + '&lng=' + lng).then(function (res) {
+    var places = res.data;
+    if (!places.length) {
+      alert('no places found!');
+      return;
+    }
+
+    var markers = places.map(function (place) {
+      // ES6 destructuring
+      var _place$location$coord = _slicedToArray(place.location.coordinates, 2),
+          placeLng = _place$location$coord[0],
+          placeLat = _place$location$coord[1];
+
+      var position = { lat: placeLat, lng: placeLng };
+      var marker = new google.maps.Marker({ map: map, position: position });
+      marker.place = place;
+      return marker;
+    });
+  });
+}
+
+function makeMap(mapDiv) {
+  // needs to be a mapdiv to run
+  if (!mapDiv) return;
+  // make our map
+  var map = new google.maps.Map(mapDiv, mapOptions);
+  loadPlaces(map);
+
+  var input = (0, _bling.$)('[name="geolocate"]');
+  console.log(input);
+  var autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+exports.default = makeMap;
 
 /***/ })
 /******/ ]);
--- public/javascripts/modules/map.js
+++ public/javascripts/modules/map.js
@@ -7,7 +7,23 @@ const mapOptions = {
 }
 
 function loadPlaces(map, lat = 43.2, lng = -79.8) {
+  axios.get(`/api/stores/near?lat=${lat}&lng=${lng}`)
+    .then(res => {
+      const places = res.data;
+      if (!places.length) {
+        alert('no places found!');
+        return;
+      }
 
+      const markers = places.map(place => {
+        // ES6 destructuring
+        const [placeLng, placeLat] = place.location.coordinates;
+        const position = { lat: placeLat, lng: placeLng };
+        const marker = new google.maps.Marker({ map, position }); 
+        marker.place = place;
+        return marker; 
+      });
+    });
 }
 
 function makeMap(mapDiv) {
@@ -15,7 +31,11 @@ function makeMap(mapDiv) {
   if (!mapDiv) return;
   // make our map
   const map = new google.maps.Map(mapDiv, mapOptions);
-  const inptut
+  loadPlaces(map);
+
+  const input = $('[name="geolocate"]');
+  console.log(input);
+  const autocomplete = new google.maps.places.Autocomplete(input);
 }
 
 
