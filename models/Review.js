d0b1e95a7fc3fac236077a980b8da97949ca0455
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-02-16 17:55:01 -0500

    fill in Schema fields for Review model

--- models/Review.js
+++ models/Review.js
@@ -2,7 +2,29 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const reviewSchema = new mongoose.Schema({
-
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: 'You must supply an author!'
+  },
+  store: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Store',
+    required: 'You must supply an store!'
+  },
+  text: {
+    type: String,
+    required: 'Your review must have text!'
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5
+  }
 })
 
-module.exports = mongoose.model('Review', reviewSchema);
+module.exports = mongoose.model('Review', reviewSchema);\
