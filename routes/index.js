0195730a1855689033f30eb5aa6220b3e4a2066a
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-05-24 01:00:39 -0400

    clean up comments in routes > index.js

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,8 @@ const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 const reviewController = require('../controllers/reviewController');
 
-// object destructuring
-// only importing the catchErrors function from errorHandlers.js
 const { catchErrors } = require('../handlers/errorHandlers');
 
-// Do work here
-// note that res and req are implicitly passed to the 2nd argument functions
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/stores/page/:page', catchErrors(storeController.getStores));
@@ -40,11 +36,6 @@ router.get('/login', userController.loginForm);
 router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 
-
-
-// 1. validate the registration data
-// 2. register the user
-// 3. we need to log them in
 router.post('/register', 
   userController.validateRegister,
   userController.register,
@@ -82,34 +73,5 @@ router.get('/top', catchErrors(storeController.getTopStores));
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
 router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
-
-
-// router.get('/', (req, res) => {
-   
-//   const peep = { name: "YO", age: 200, cool: true };
-//   res.send('Hey! It works!');
-//   res.json(peep);
-
-//   sends back 'name=' data entered into url query
-//   res.send(req.query.name);
-
-//   sends back url query input as a json string
-//   res.send(req.query) actually sends back the same thing...
-//   res.json(req.query);
-
-//   res.render('hello', {
-//     name: req.query.name,
-//     dog: req.query.dog,
-//     title: "I love ramen"
-//   });
-// });
-
-// router example of handling url inputs
-// reverses name entered at '/reverse/_____'
-// router.get('/reverse/:name', (req, res) => {
-//   let name = [...req.params.name];
-//   res.send(name.reverse().join("")) 
-  
-// });
 
 module.exports = router;
